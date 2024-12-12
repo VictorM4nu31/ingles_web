e803db4e534aa6d6136a369f2ff767b4569a2706
layronBot <gutierrezlugolayron046@gmail.com>
2024-12-12 16:47:17 -0600

    Visualización de las lecciones

--- unidades/unidad10.php
+++ unidades/unidad10.php
@@ -3,14 +3,20 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Unidad 10</title>
 </head>
 <body>
     <h1>Bienvenido a la Unidad 10. Music</h1>
-    <button onclick="window.location.href='unit10/lesson_a.php'">Lesson A</button>
-    <button onclick="window.location.href='unit10/lesson_b.php'">Lesson B</button>
-    <button onclick="window.location.href='unit10/lesson_c.php'">Lesson C</button>
-    <button onclick="window.location.href='unit10/lesson_d.php'">Lesson D</button>
-    <button onclick="window.location.href='index.php'">Regresar a la Pantalla Principal</button>
+    
+    <h3>Por favor selecciona la lección que deseas.</h3>
+    <!-- Botones para las lecciones de la unidad 10 -->
+    <div class="text-center mt-4">
+        <button onclick="window.location.href='unit10/lesson_a.php'" class="btn btn-primary">Lección A</button>
+        <button onclick="window.location.href='unit10/lesson_b.php'" class="btn btn-primary">Lección B</button>
+        <button onclick="window.location.href='unit10/lesson_c.php'" class="btn btn-primary">Lección C</button>
+        <button onclick="window.location.href='unit10/lesson_d.php'" class="btn btn-primary">Lección D</button>
+    </div>
+
+    <button onclick="window.location.href='../index.php'" class="btn btn-secondary">Regresar a la Pantalla Principal</button>
 </body>
 </html>
--- unidades/unidad11.php
+++ unidades/unidad11.php
@@ -3,13 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Unidad 11</title>
 </head>
 <body>
     <h1>Bienvenido a la Unidad 11. Living your life</h1>
-    <button>Lesson A</button>
-    <button>Lesson B</button>
-    <button>Lesson C</button>
-    <button>Lesson D</button>
+    <div class="text-center mt-4">
+        <button onclick="window.location.href='unit11/lesson_a.php'" class="btn btn-primary">Lección A</button>
+        <button onclick="window.location.href='unit11/lesson_b.php'" class="btn btn-primary">Lección B</button>
+        <button onclick="window.location.href='unit11/lesson_c.php'" class="btn btn-primary">Lección C</button>
+        <button onclick="window.location.href='unit11/lesson_d.php'" class="btn btn-primary">Lección D</button>
+    </div>
+
+    <button onclick="window.location.href='index.php'" class="btn btn-secondary">Regresar a la Pantalla Principal</button>
 </body>
 </html>
